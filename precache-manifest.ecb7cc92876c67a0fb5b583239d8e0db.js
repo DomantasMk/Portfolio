4214dca4ab75fc85fb94e4998eedb9b8d1b8fe5c
DomantasMk <domantas.mikelionis@gmail.com>
2020-09-27 22:36:51 +0300

    Updates

---
+++ precache-manifest.ecb7cc92876c67a0fb5b583239d8e0db.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32096819e6cebf3135acd9eef9be3816",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b47dfdb6c55286af0c82",
+    "url": "/Portfolio/static/css/main.92155083.chunk.css"
+  },
+  {
+    "revision": "ca79806668fbae2d2e70",
+    "url": "/Portfolio/static/js/2.c48ab5b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.c48ab5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47dfdb6c55286af0c82",
+    "url": "/Portfolio/static/js/main.48e7ca19.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "bb50013e2c2abd338e96d57e7625ab70",
+    "url": "/Portfolio/static/media/Domantas_Mikelionis_CV.bb50013e.pdf"
+  },
+  {
+    "revision": "2e38882fa09b0ceae72c577a21272127",
+    "url": "/Portfolio/static/media/HandBookAndroid.2e38882f.gif"
+  },
+  {
+    "revision": "7134360810d10f5f531489a775555d7f",
+    "url": "/Portfolio/static/media/HandBookIOS.71343608.gif"
+  },
+  {
+    "revision": "713c5a46149c439df032075945a9939f",
+    "url": "/Portfolio/static/media/LTJobStatistics.713c5a46.png"
+  },
+  {
+    "revision": "9d2ed9f789a2a22f57aa67b6a2fad124",
+    "url": "/Portfolio/static/media/ProfilePic.9d2ed9f7.jpg"
+  },
+  {
+    "revision": "15d0dfc68423c8d70ff16229f10962ea",
+    "url": "/Portfolio/static/media/ReflixAppBrowse.15d0dfc6.gif"
+  },
+  {
+    "revision": "1451e906a2d12fee769240937ce5c4ab",
+    "url": "/Portfolio/static/media/ReflixAppLibrary.1451e906.gif"
+  },
+  {
+    "revision": "b0ab048a587492d3e5fffd837790fee3",
+    "url": "/Portfolio/static/media/ReflixAppSearch.b0ab048a.gif"
+  },
+  {
+    "revision": "aaa45b918d1a4941c03d694c843bc713",
+    "url": "/Portfolio/static/media/TimeBoxItMainView.aaa45b91.png"
+  }
+]);
